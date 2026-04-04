6e2886c800e308d8fde9876bf66c564a7be0ce13
Sawkez <sav.chirkow2015@gmail.com>
2026-04-04 23:33:26 +0400

    exit build scripts on failure

--- build/scripts/psp-pbp.sh
+++ build/scripts/psp-pbp.sh
@@ -5,12 +5,12 @@ SRC_DIR=$SCRIPT_DIR/../..
 BUILD_DIR=$SCRIPT_DIR/../build-files/psp-pbp
 EXPORT_DIR=$SCRIPT_DIR/../export/psp-pbp
 
-mkdir -p $BUILD_DIR
-psp-cmake -S $SRC_DIR -B $BUILD_DIR -DCMAKE_BUILD_TYPE=Debug
-cmake --build $BUILD_DIR
+mkdir -p $BUILD_DIR || exit 1
+psp-cmake -S $SRC_DIR -B $BUILD_DIR -DCMAKE_BUILD_TYPE=Debug || exit 1
+cmake --build $BUILD_DIR || exit 1
 
-mkdir -p $EXPORT_DIR
-rm -rf $EXPORT_DIR/*
-cp $BUILD_DIR/EBOOT.PBP $EXPORT_DIR
-cp -r $SRC_DIR/content $EXPORT_DIR/
-cp -r $SRC_DIR/mods $EXPORT_DIR/
+mkdir -p $EXPORT_DIR || exit 1
+rm -rf $EXPORT_DIR/* || exit 1
+cp $BUILD_DIR/EBOOT.PBP $EXPORT_DIR || exit 1
+cp -r $SRC_DIR/content $EXPORT_DIR/ || exit 1
+cp -r $SRC_DIR/mods $EXPORT_DIR/ || exit 1
