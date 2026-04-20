9689723c359ee1786b40463900408a9b8122a276
Sawkez <sav.chirkow2015@gmail.com>
2026-04-20 10:24:37 +0400

    add SaveManagerAndroid placeholder

--- code/engine/save/SaveManager.hpp
+++ code/engine/save/SaveManager.hpp
@@ -4,6 +4,8 @@
 
 #if SDL_PLATFORM_PSP
 #include "engine/save/SaveManagerPSP.hpp"
+#elif SDL_PLATFORM_ANDROID
+#include "engine/save/SaveManagerAndroid.hpp"
 #else
 #include "engine/save/SaveManagerPC.hpp"
 #endif
@@ -14,6 +16,8 @@ static SaveManagerBase* instance;
 void Init() {
 #if SDL_PLATFORM_PSP
 	instance = new SaveManagerPSP;
+#elif SDL_PLATFORM_ANDROID
+	instance = new SaveManagerAndroid;
 #else
 	instance = new SaveManagerPC;
 #endif
---
+++ code/engine/save/SaveManagerAndroid.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <ctime>
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+
+#include "engine/save/ISaveManagerPC.hpp"
+#include "engine/save/SaveManagerBase.hpp"
+#include "engine/ui/UIManager.hpp"
+#include "game/ui/save/ListLoadMenu.hpp"
+#include "game/ui/save/ListSaveMenu.hpp"
+
+class SaveManagerAndroid : public SaveManagerBase {
+	void Init() override {}
+	void ShowSaveMenu() override {}
+	void ShowLoadMenu() override {}
+	void Autosave() override {}
+	void Draw() override {}
+	bool OverrideDrawing() const override {}
+};
