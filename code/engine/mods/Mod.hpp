2d79925bede65a10052357be065fd417533299a2
Sawkez <sav.chirkow2015@gmail.com>
2026-07-09 09:48:33 +0400

    rewrite mods to use SDL_Storage

--- code/engine/mods/Mod.hpp
+++ code/engine/mods/Mod.hpp
@@ -9,13 +9,24 @@ class Mod {
    private:
 	std::string _path;
 	std::string _name;
+	SDL_Storage* _storage;
 
    public:
-	Mod(const std::string& path, yyjson_val* json) : _path(path), _name(yyjson_get_str(yyjson_obj_get(json, "name"))) {}
+	Mod(const std::string& path, SDL_Storage* storage, yyjson_val* json)
+		: _path(path), _name(yyjson_get_str(yyjson_obj_get(json, "name"))), _storage(storage) {}
 
-	Mod(const std::string& path, yyjson_doc* doc) : Mod(path, yyjson_doc_get_root(doc)) { yyjson_doc_free(doc); }
-	Mod(const std::string& path) : Mod(path, ResourceManager::LoadJson(path + "/mod.json")) {}
+	Mod(const std::string& path, SDL_Storage* storage, yyjson_doc* doc) : Mod(path, yyjson_doc_get_root(doc)) {
+		yyjson_doc_free(doc);
+	}
 
-	const std::string& GetName() const { return _name; }
+	Mod(const std::string& path, SDL_Storage* storage)
+		: Mod(path, storage, ResourceManager::LoadJson(storage, "mod.json")) {}
+
+	Mod(const std::string& path) : Mod(path, SDL_OpenTitleStorage(path.c_str(), 0)) {}
+
+	~Mod() { SDL_CloseStorage(_storage); }
+
 	const std::string& GetPath() const { return _path; }
+
+	SDL_Storage* GetStorage() { return _storage; }
 };
