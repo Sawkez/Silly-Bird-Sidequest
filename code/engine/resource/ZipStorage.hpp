3cf5c9c93c62e69d066ca15917be320b14517178
Sawkez <sav.chirkow2015@gmail.com>
2026-07-10 02:59:42 +0400

    use stream zips

--- code/engine/resource/ZipStorage.hpp
+++ code/engine/resource/ZipStorage.hpp
@@ -7,41 +7,42 @@
 #include <string>
 
 #include "engine/devconsole/DevConsole.hpp"
+#include "engine/resource/ResourceManager.hpp"
+#include "engine/resource/ZipStream.hpp"
 #include "zip.h"
 
 namespace ZipStorage {
-
 inline SDL_StorageInterface interface;
 
 SDL_Storage* Open(const std::string& path) {
-	zip_t* zip = zip_open(path.c_str(), 0, 'r');
-	if (zip == nullptr) {
-		// dc::err << "ERROR opening zip " << path << ": " << zip_strerror(error) << dc::endl;
+	auto* zip = new ZipStream(path);
+
+	if (zip->stream == nullptr) {
 		return nullptr;
 	}
 
-	int count = zip_entries_total(zip);
+	int count = zip_entries_total(zip->zip);
 
 	dc::msg << "Contents of " << path << ":" << dc::endl;
 
 	for (int i = 0; i < count; i++) {
-		zip_entry_openbyindex(zip, i);
-		dc::msg << zip_entry_name(zip) << dc::endl;
-		zip_entry_close(zip);
+		zip_entry_openbyindex(zip->zip, i);
+		dc::msg << zip_entry_name(zip->zip) << dc::endl;
+		zip_entry_close(zip->zip);
 	}
 
 	return SDL_OpenStorage(&interface, zip);
 }
 
 bool Close(void* userData) {
-	zip_close((zip_t*)userData);
+	delete (ZipStream*)userData;
 	return true;
 }
 
 bool Ready(void* userData) { return true; }
 
 bool Enumerate(void* userData, const char* directory, SDL_EnumerateDirectoryCallback callback, void* callbackUserdata) {
-	auto* zip = (zip_t*)userData;
+	auto* zip = ((ZipStream*)userData)->zip;
 	int count = zip_entries_total(zip);
 
 	for (int i = 0; i < count; i++) {
@@ -89,7 +90,7 @@ bool Enumerate(void* userData, const char* directory, SDL_EnumerateDirectoryCall
 }
 
 bool Info(void* userData, const char* path, SDL_PathInfo* info) {
-	auto* zip = (zip_t*)userData;
+	auto* zip = ((ZipStream*)userData)->zip;
 	if (zip_entry_open(zip, path) < 0) return false;
 
 	info->type = zip_entry_isdir(zip) ? SDL_PATHTYPE_DIRECTORY : SDL_PATHTYPE_FILE;
@@ -103,7 +104,7 @@ bool Info(void* userData, const char* path, SDL_PathInfo* info) {
 }
 
 bool ReadFile(void* userData, const char* path, void* destination, Uint64 length) {
-	auto* zip = (zip_t*)userData;
+	auto* zip = ((ZipStream*)userData)->zip;
 	if (zip_entry_open(zip, path) < 0) return false;
 
 	zip_entry_noallocread(zip, destination, length);
