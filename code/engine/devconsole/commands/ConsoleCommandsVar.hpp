f0fb5b9ebcf3dc49a83bdfd84d12d172a9faf58a
Sawkez <sav.chirkow2015@gmail.com>
2026-08-25 22:07:43 +0400

    add error checking to set command

--- code/engine/devconsole/commands/ConsoleCommandsVar.hpp
+++ code/engine/devconsole/commands/ConsoleCommandsVar.hpp
@@ -8,6 +8,11 @@
 namespace ConsoleCommandsVar {
 
 void set(const std::vector<std::string>& args, bool fromUser) {
+	if (args.size() < 2) {
+		dc::err << "Usage: set <VARIABLE_NAME> <value>" << dc::endl;
+		return;
+	}
+
 	DevConsoleVariableManager::Set(args[0], args[1], fromUser);
 }
 }  // namespace ConsoleCommandsVar
