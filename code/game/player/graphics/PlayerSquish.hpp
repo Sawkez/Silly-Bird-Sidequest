994220a27dfb501503b9ed93091e06c228f66648
Sawkez <sav.chirkow2015@gmail.com>
2026-08-25 23:01:01 +0400

    use convars in duck state

---
+++ code/game/player/graphics/PlayerSquish.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "engine/devconsole/DevConsoleFlags.hpp"
+#include "engine/devconsole/variable/DevConsoleVariable.hpp"
+
+#define CONVAR_CATEGORY PLAYER_SQUISH
+
+namespace PlayerSquish {
+// clang-format off
+    CONVAR(float,   duck,       DUCK,       0.5f,   DC_FLAG_NONE);
+    CONVAR(float,   standUp,    STAND_UP,   1.5f,   DC_FLAG_NONE);
+    CONVAR(float,   twerkDown,  TWERK_DOWN, 0.9f,   DC_FLAG_NONE);
+    CONVAR(float,   twerkUp,    TWERK_UP,   1.1f,   DC_FLAG_NONE);
+// clang-format on
+}  // namespace PlayerSquish
+
+#undef CONVAR_CATEGORY
